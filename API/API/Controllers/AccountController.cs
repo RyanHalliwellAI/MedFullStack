05aaa776d553323f3e71324736042a9acf389ca1
RyanHalliwellAI <ryan.halliwellai@gmail.com>
2023-04-04 16:43:21 +0930

    implemnting CreateAccount method

--- API/API/Controllers/AccountController.cs
+++ API/API/Controllers/AccountController.cs
@@ -19,15 +19,43 @@ namespace API.Controllers
         [HttpPost("create")]
         public async Task<IActionResult> CreateAccount([FromBody] AccountRequest request)
         {
-            return ("");
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                await connection.OpenAsync();
+
+                // Check if the username or email exists in Database
+                var checkQuery = "SELECT COUNT(*) FROM Users WHERE Username = @Username OR Email = @Email";
+                using (var command = new SqlCommand(checkQuery, connection))
+                {
+                    command.Parameters.AddWithValue("@Username", request.Username);
+                    command.Parameters.AddWithValue("@Email", request.Email);
+
+                    var count = (int)await command.ExecuteScalarAsync();
+                    if (count > 0)
+                    {
+                        return BadRequest("Account name or email already exists within system");
+                    }
+                }
+                // Insert new user to Database
+                var insertQuery = "INSERT INTO Users (Username, Email, Password) VALUES (@Username, @Email, @Password)";
+                using (var command = new SqlCommand(insertQuery, connection))
+                {
+                    command.Parameters.AddWithValue("@Username", request.Username);
+                    command.Parameters.AddWithValue("@Email", request.Email);
+                    command.Parameters.AddWithValue("@Password", request.Password); //will hash later
+
+                    await command.ExecuteNonQueryAsync();
+                }
+
+            }
+            return Ok("Account created!");
+
         }
-
-
-    }
-    public class AccountRequest
-    {
-        public string Username { get; set; }
-        public string Email { get; set; }
-        public string Password { get; set; }
+        public class AccountRequest
+        {
+            public string Username { get; set; }
+            public string Email { get; set; }
+            public string Password { get; set; }
+        }
     }
 }
