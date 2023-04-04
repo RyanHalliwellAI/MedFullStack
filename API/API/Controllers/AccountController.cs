d31f53ed5bde6812192c982310e91dd53a24c385
RyanHalliwellAI <ryan.halliwellai@gmail.com>
2023-04-04 10:20:03 +0930

    Adding backend for login

---
+++ API/API/Controllers/AccountController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly string _connectionString;
+
+        public AccountController(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("MedAppCon");
+        }
+
+        [HttpPost("create")]
+        public async Task<IActionResult> CreateAccount([FromBody] AccountRequest request)
+        {
+            return ("");
+        }
+
+
+    }
+    public class AccountRequest
+    {
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
--- API/API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa27fab65062f7ac2dc3f65733a7759617ced935")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9864278bddd95454212882d3484d96f4529384d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
