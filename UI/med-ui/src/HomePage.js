aa5a6c7fcbff072a6844b5ee5409411587449031
RyanHalliwellAI <ryan.halliwellai@gmail.com>
2023-04-09 12:21:49 +0930

    Adding log out feature

--- UI/med-ui/src/HomePage.js
+++ UI/med-ui/src/HomePage.js
@@ -1,10 +1,14 @@
 import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Home() {
     const [message, setMessage] = useState('Welcome!');
+    const navigate = useNavigate();
 
+
+    //gets login from local storage
     useEffect(() => {
       const userEmail = localStorage.getItem('userEmail');
       if (userEmail) {
@@ -12,14 +16,23 @@ function Home() {
       }
     }, []);
 
+    //handling the logout logic.
+    const handleLogout = () => {
+        localStorage.removeItem('userEmail');
+        navigate('/login');
+      };
 
 
-    <h1>Test</h1>
+
+    <h1>Home Page</h1>
     return (
         <div className="App container">
             <h3 className="d-flex justify-content-center m-3">Home</h3>
             <div className="d-flex justify-content-center">
                 <p>{message}</p>
+            </div>
+            <div className="d-flex justify-content-center">
+                <button className="btn btn-danger" onClick={handleLogout}>Logout</button>
             </div>
         </div>
     );
