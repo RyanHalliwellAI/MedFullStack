66424f681fe1b803cd20ed669b57bba2b5ae056e
RyanHalliwellAI <ryan.halliwellai@gmail.com>
2023-05-02 14:30:20 +0930

    Adding Ai logic

--- UI/med-ui/src/app.py
+++ UI/med-ui/src/app.py
@@ -2,10 +2,26 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
+def check_symptoms(symptoms):
+    symptoms = symptoms.lower()
+    
+    if "fever" in symptoms and "cough" in symptoms and "fatigue" in symptoms:
+        return "You may have the flu."
+    elif "sneezing" in symptoms and "itchy eyes" in symptoms and "runny nose" in symptoms:
+        return "You may have allergies."
+    elif "fever" in symptoms and "dry cough" in symptoms and "loss of taste" in symptoms:
+        return "You may have COVID-19."
+    elif "sore throat" in symptoms and "runny nose" in symptoms and "mild fever" in symptoms:
+        return "You may have a common cold."
+    else:
+        return "Your symptoms are not recognized. Please consult a doctor."
+
 @app.route('/process_symptoms', methods=['POST'])
 def process_symptoms():
     data = request.json
     symptoms = data.get('symptoms')
+
+    result = check_symptoms(symptoms)
     
     return jsonify({'result': result})
 
